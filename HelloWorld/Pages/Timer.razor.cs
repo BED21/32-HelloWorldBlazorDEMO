270368bb2c45f7396442207589f813c8c5980e2b
Claes Engelin <claes.engelin@zocom.se>
2022-04-13 11:56:42 +0200

    Timer refactored
    Put all C# code in a separate file behind Timer.razor

---
+++ HelloWorld/Pages/Timer.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HelloWorld.Pages;
+
+public class TimerBase : ComponentBase, IDisposable
+{
+    private int timeLeft = 25 * 60; // Återstående tid, i sekunder
+    protected string remaining => TimeSpan.FromSeconds(timeLeft).ToString(@"mm\:ss");
+
+    PeriodicTimer? timer;
+
+    protected async Task Start()
+    {
+        timer?.Dispose();
+        timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+        while (await timer.WaitForNextTickAsync())
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= 1;
+                //Console.WriteLine("timer ticked");
+                await InvokeAsync(StateHasChanged);
+            }
+        }
+    }
+
+    protected void Stop()
+    {
+        timer?.Dispose();
+    }
+
+    public void Dispose()
+    {
+        timer?.Dispose();
+    }
+}
